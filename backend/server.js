3cfbc014cf6948c447eb18082052389416aeb24d
adityaw77 <adityawagh2k4@gmail.com>
2026-06-24 13:40:59 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
 const issueRoutes = require('./routes/issueRoutes');
-
 const app = express();
+const PORT = 5001;
 
 app.use(cors());
 app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/issueDB')
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 app.use('/api/issues', issueRoutes);
-app.listen(PORT,() => {
-    console.log(`Server running on port ${PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
